9111bd0acd66b5a38ad0bfcaf3fbd938eaba86f6
unhega <unhega@gmail.com>
2018-11-15 11:48:11 +0700

    Change json provider

--- Services/CryptoCompareService.cs
+++ Services/CryptoCompareService.cs
@@ -1,22 +1,24 @@
 using System.Net.Http;
 using System.Threading.Tasks;
-using System.Runtime.Serialization.Json;
 using Microsoft.EntityFrameworkCore;
 using sqlitedbapp.Models;
 using System;
 using System.Threading;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace sqlitedbapp.Services
 {
-    public class CryptoCompareService: IDisposable
+    public class CryptoCompareService : IDisposable
     {
         IServiceProvider serviceProvider;
         ILogger logger;
         HttpClient client;
         CancellationTokenSource tokenSource;
-        public CryptoCompareService(IServiceProvider serviceProvider, ILogger<CryptoCompareService> logger){
+        public CryptoCompareService(IServiceProvider serviceProvider, ILogger<CryptoCompareService> logger)
+        {
             this.serviceProvider = serviceProvider;
             this.logger = logger;
             client = new HttpClient();
@@ -28,23 +30,26 @@ namespace sqlitedbapp.Services
             tokenSource.Dispose();
         }
 
-        public void Process(string apiuri){
+        public void Process(string apiuri)
+        {
             tokenSource = new CancellationTokenSource();
             ProcessAsync(apiuri, tokenSource.Token);
         }
 
-        private async void ProcessAsync(string apiuri, CancellationToken token){
-            while(!token.IsCancellationRequested){
-                var responseStream = client.GetStreamAsync(apiuri);
-                var serializer = new DataContractJsonSerializer(typeof(Price));
-                var price = serializer.ReadObject(await responseStream) as Price;
-                using(var _scope = serviceProvider.CreateScope())
-                using(var dbContext = _scope.ServiceProvider.GetRequiredService<SqliteDbContext>())
+        private async void ProcessAsync(string apiuri, CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                var response = client.GetStringAsync(apiuri);
+                var price = JsonConvert.DeserializeObject<Price>(await response) as Price;
+
+                using (var _scope = serviceProvider.CreateScope())
+                using (var dbContext = _scope.ServiceProvider.GetRequiredService<SqliteDbContext>())
                 {
                     await dbContext.AddAsync(price);
                     await dbContext.SaveChangesAsync();
                 }
-                    
+
                 logger.LogInformation(price.ToString());
                 await Task.Delay(15000);
             }
